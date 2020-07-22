33ff84a645c3284f35597ac51b721fc26b9d0ae5
Karega K. Scott <kkscott@outlook.com>
2020-07-21 20:01:13 -0500

    Remove import submodule references

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,7 +1,6 @@
 import { configure, render } from '@testing-library/react';
 import * as React from "react";
 import renderer from "react-test-renderer";
-import NavLinks from "../rescueshelter/src/components/shared/navlinks";
 
 configure({ testIdAttribute: 'App' });
 
